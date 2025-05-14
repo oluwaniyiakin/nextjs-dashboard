ab3d45ae3bde624ed20fc46cafee55ce8b88ace3
OLUWANIYIAKIN <olu24002@byui.edu>
2025-05-14 13:41:38 +0100

    Initial commit for Next.js dashboard project

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+// app/dashboard/page.tsx
+export default function Page() {
+    return <p>Dashboard Page</p>;
+  }
---
+++ app/dashboard/sidenav.tsx
@@ -0,0 +1,9 @@
+// app/ui/dashboard/sidenav.tsx
+export default function SideNav() {
+    return (
+      <nav className="bg-gray-200 h-full p-4">
+        <p>Side Navigation</p>
+      </nav>
+    );
+  }
+  
