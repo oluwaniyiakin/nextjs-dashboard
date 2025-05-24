8bf0dc073c35d10991a3731638a931a37adf627f
OLUWANIYIAKIN <olu24002@byui.edu>
2025-05-24 17:32:32 +0100

    HH

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,12 +1,10 @@
 // /app/dashboard/page.tsx
 
 import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
-import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+
 
 export const dynamic = 'force-dynamic';
 
